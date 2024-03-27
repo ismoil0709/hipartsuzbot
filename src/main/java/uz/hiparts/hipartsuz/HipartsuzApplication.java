627bf26bd70860c95bf878a5eced95a1337527b7
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-03-27 23:24:23 +0500

    first commit

---
+++ src/main/java/uz/hiparts/hipartsuz/HipartsuzApplication.java
@@ -0,0 +1,13 @@
+package uz.hiparts.hipartsuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HipartsuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HipartsuzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/hiparts/hipartsuz/HipartsuzApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.hiparts.hipartsuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HipartsuzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
