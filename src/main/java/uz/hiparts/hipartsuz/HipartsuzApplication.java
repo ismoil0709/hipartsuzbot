0d5c306169decf0e7a8093968fd0160e231bb179
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-04-07 23:04:35 +0500

    ngrok added

--- src/main/java/uz/hiparts/hipartsuz/HipartsuzApplication.java
+++ src/main/java/uz/hiparts/hipartsuz/HipartsuzApplication.java
@@ -3,9 +3,13 @@ package uz.hiparts.hipartsuz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class HipartsuzApplication {
-    public static void main(String[] args) {
+    private static final String NGROK_URL = "loved-basilisk-magnetic.ngrok-free.app";
+    public static void main(String[] args) throws IOException {
+        new ProcessBuilder("/snap/bin/ngrok", "http", "--domain=" + NGROK_URL, "8080").start();
         SpringApplication.run(HipartsuzApplication.class,args);
     }
 }
