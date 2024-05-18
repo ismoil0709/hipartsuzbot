f74111e62846d6b1a397c0bf61c019f0259900c7
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-05-18 21:49:02 +0500

    removed bot token from git

--- src/main/java/uz/hiparts/hipartsuz/util/BotUtils.java
+++ src/main/java/uz/hiparts/hipartsuz/util/BotUtils.java
@@ -25,7 +25,7 @@ import java.util.UUID;
 public class BotUtils {
     private static final String BASE_URL = "https://api.telegram.org/bot";
     private static final String FILE_URL = "https://api.telegram.org/file/bot";
-    private static final String BOT_TOKEN = "6674985300:AAFgJYmO42YggQBFBY1450zkkHmZj7QZGjM/";
+    private static final String BOT_TOKEN = "<bot_token>/";
     private static final RestTemplate restTemplate = new RestTemplate();
     public static <T extends Serializable,Method extends BotApiMethod<T>> void send(Method method) {
         restTemplate.postForObject(BASE_URL + BOT_TOKEN + method.getMethod(),method, TelegramResultDto.class);
