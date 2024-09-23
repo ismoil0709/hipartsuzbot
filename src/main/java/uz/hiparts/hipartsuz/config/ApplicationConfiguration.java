4f500ceeabb58a9fd59836e181c3556f3b7743a8
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-09-23 15:19:00 +0500

    some problems fixed

--- src/main/java/uz/hiparts/hipartsuz/config/ApplicationConfiguration.java
+++ src/main/java/uz/hiparts/hipartsuz/config/ApplicationConfiguration.java
@@ -14,9 +14,7 @@ import org.springframework.web.filter.CorsFilter;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import uz.hiparts.hipartsuz.util.BotUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 
 @Configuration
@@ -24,6 +22,7 @@ import java.io.InputStreamReader;
 @Slf4j
 public class ApplicationConfiguration {
     private final Environment env;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -35,46 +34,20 @@ public class ApplicationConfiguration {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
     @Bean
-    public ResourceBundleMessageSource messageSourceResourceBundle(){
+    public ResourceBundleMessageSource messageSourceResourceBundle() {
         ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
         resourceBundleMessageSource.setBasename("messages");
         resourceBundleMessageSource.setFallbackToSystemLocale(false);
         resourceBundleMessageSource.setDefaultEncoding(CharEncoding.UTF_8);
         return resourceBundleMessageSource;
     }
+
     @PostConstruct
     public void init() throws IOException {
-        String ngrokUrl = env.getProperty("bot.data.ngrok_url");
-        String serverPort = env.getProperty("server.port");
-        String ngrokPath = null;
-        String command = "which";
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            command = "where";
-        }
-        try {
-            Process process = new ProcessBuilder(command, "ngrok").start();
-            process.waitFor();
-            int exitCode = process.exitValue();
-            if (exitCode == 0) {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-                ngrokPath = reader.readLine();
-                reader.close();
-            } else {
-                log.error("Failed to locate ngrok using 'which ngrok'. Please ensure ngrok is installed and accessible in your system PATH.");
-                System.exit(0);
-            }
-        } catch (IOException | InterruptedException e) {
-            log.error("Error while finding ngrok path: " + e.getMessage());
-            System.exit(0);
-        }
-        if (ngrokPath == null || ngrokUrl == null || serverPort == null) {
-            log.error("Missing required properties: ngrok_url, server_port, or ngrok not found. Ngrok tunnel creation skipped.");
-            System.exit(0);
-        }else {
-            Process process = new ProcessBuilder(ngrokPath,"http","--domain=" + ngrokUrl,serverPort).start();
-            if (!BotUtils.getWebhookUrl().equals("https://" + ngrokUrl + "/api/v1/telegram"))
-                BotUtils.send(SetWebhook.builder().url("https://" + ngrokUrl + "/api/v1/telegram").build());
-        }
+        if (!BotUtils.getWebhookUrl().equals("https://hipartsbot.uz:8095/api/v1/telegram"))
+            BotUtils.send(SetWebhook.builder().url("https://hipartsbot.uz:8095/api/v1/telegram").build());
     }
+
 }
