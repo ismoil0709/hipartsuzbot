6b1da9a969a87e9cdd338379d2c7571362cecd90
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-09-25 16:28:56 +0500

    test

--- src/main/java/uz/hiparts/hipartsuz/controller/TelegramController.java
+++ src/main/java/uz/hiparts/hipartsuz/controller/TelegramController.java
@@ -1,11 +1,13 @@
 package uz.hiparts.hipartsuz.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import uz.hiparts.hipartsuz.dto.CompleteResponseDto;
 import uz.hiparts.hipartsuz.dto.PrepareRequestDto;
 import uz.hiparts.hipartsuz.model.TelegramUser;
 import uz.hiparts.hipartsuz.service.TelegramUserService;
@@ -29,7 +31,8 @@ public class TelegramController {
         }
     }
     @PostMapping("/click/prepare")
-    public void prepare(@RequestBody PrepareRequestDto dto){
+    public ResponseEntity<?> prepare(@RequestBody PrepareRequestDto dto){
         System.out.println(dto.toString());
+        return ResponseEntity.ok(new CompleteResponseDto(dto.getClickTransId(),dto.getMerchantTransId(),null,0,""));
     }
 }
