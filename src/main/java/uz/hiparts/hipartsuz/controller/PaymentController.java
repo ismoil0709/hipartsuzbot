bb16d53d080326e36c6a320306a9c9c1156063d1
ツ <javaguru1716@gmail.com>
2025-02-27 14:50:02 +0500

    minor fix

--- src/main/java/uz/hiparts/hipartsuz/controller/PaymentController.java
+++ src/main/java/uz/hiparts/hipartsuz/controller/PaymentController.java
@@ -24,7 +24,7 @@ public class PaymentController {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ClickDto prepareClick(@RequestParam Map<String, Object> request){
-        return paymentServiceClick.prepare(mapRequestToDto(request));
+        return paymentServiceClick.prepare(new ClickDto(request));
     }
 
     @PostMapping(value = "/click/complete",
@@ -32,7 +32,7 @@ public class PaymentController {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ClickDto completeClick(@RequestParam Map<String,Object> request){
-        return paymentServiceClick.complete(mapRequestToDto(request));
+        return paymentServiceClick.complete(new ClickDto(request));
     }
 
     @PostMapping("/payme")
@@ -40,25 +40,4 @@ public class PaymentController {
                     @RequestHeader(value = "Authorization",required = false) String authorization) {
         return paymentServicePayme.payWithPaycom(requestForm, authorization);
     }
-
-    private ClickDto mapRequestToDto(Map<String, Object> request) {
-        ClickDto dto = new ClickDto();
-
-        dto.setClickTransId(request.containsKey("click_trans_id") ? Long.valueOf(request.get("click_trans_id").toString()) : null);
-        dto.setServiceId(request.containsKey("service_id") ? Long.valueOf(request.get("service_id").toString()) : null);
-        dto.setClickPaydocId(request.containsKey("click_paydoc_id") ? Long.valueOf(request.get("click_paydoc_id").toString()) : null);
-        dto.setMerchantTransId(request.containsKey("merchant_trans_id") ? request.get("merchant_trans_id").toString() : null);
-        dto.setMerchantPrepareId(request.containsKey("merchant_prepare_id") ? Long.valueOf(request.get("merchant_prepare_id").toString()) : null);
-        dto.setMerchantConfirmId(request.containsKey("merchant_confirm_id") ? Long.valueOf(request.get("merchant_confirm_id").toString()) : null);
-        dto.setAmount(request.containsKey("amount") ? Float.parseFloat(request.get("amount").toString()) : 0.0f);
-        dto.setAction(request.containsKey("action") ? Long.valueOf(request.get("action").toString()) : null);
-        dto.setError(request.containsKey("error") ? Long.valueOf(request.get("error").toString()) : null);
-        dto.setErrorNote(request.containsKey("error_note") ? request.get("error_note").toString() : null);
-        dto.setSignTime(request.containsKey("sign_time") ? request.get("sign_time").toString() : null);
-        dto.setSignString(request.containsKey("sign_string") ? request.get("sign_string").toString() : null);
-
-        return dto;
-    }
-
-
 }
--- src/main/java/uz/hiparts/hipartsuz/dto/ClickDto.java
+++ src/main/java/uz/hiparts/hipartsuz/dto/ClickDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import java.util.Map;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -46,4 +48,19 @@ public class ClickDto {
 
     @JsonProperty(value = "sign_string")
     private String signString;
+
+    public ClickDto(Map<String, Object> request) {
+        this.clickTransId = request.containsKey("click_trans_id") ? Long.valueOf(request.get("click_trans_id").toString()) : null;
+        this.serviceId = request.containsKey("service_id") ? Long.valueOf(request.get("service_id").toString()) : null;
+        this.clickPaydocId = request.containsKey("click_paydoc_id") ? Long.valueOf(request.get("click_paydoc_id").toString()) : null;
+        this.merchantTransId = request.containsKey("merchant_trans_id") ? request.get("merchant_trans_id").toString() : null;
+        this.merchantPrepareId = request.containsKey("merchant_prepare_id") ? Long.valueOf(request.get("merchant_prepare_id").toString()) : null;
+        this.merchantConfirmId = request.containsKey("merchant_confirm_id") ? Long.valueOf(request.get("merchant_confirm_id").toString()) : null;
+        this.amount = request.containsKey("amount") ? Float.parseFloat(request.get("amount").toString()) : 0.0f;
+        this.action = request.containsKey("action") ? Long.valueOf(request.get("action").toString()) : null;
+        this.error = request.containsKey("error") ? Long.valueOf(request.get("error").toString()) : null;
+        this.errorNote = request.containsKey("error_note") ? request.get("error_note").toString() : null;
+        this.signTime = request.containsKey("sign_time") ? request.get("sign_time").toString() : null;
+        this.signString = request.containsKey("sign_string") ? request.get("sign_string").toString() : null;
+    }
 }
